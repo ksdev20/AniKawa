f4ee6e9242305275f7a03d426664cabee4ca966d
ksdev20 <batbale01@gmail.com>
2025-08-12 13:02:54 +0530

    sitemap tweaks :/

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -4,23 +4,24 @@ import getAllUrlsForSitemap from "../filters/getAnimeForSitemap";
 export async function GET() {
     const urls = await getAllUrlsForSitemap();
 
-    const finalUrls = urls.map(({ loc, lastmod }) => `
-        <url>
-            <loc>${loc}</loc>
-            <lastmod>${lastmod}</lastmod>
-            <priority>0.8</priority>
-        </url>
-    `).join('\n');
+    const finalUrls = urls.map(({ loc, lastmod }) => (
+`  <url>
+    <loc>${loc}</loc>
+    <lastmod>${lastmod}</lastmod>
+    <priority>0.8</priority>
+  </url>`
+    )).join("\n");
 
-    const xml = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${finalUrls}
-    </urlset>`;
+    const xml =
+`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${finalUrls}
+</urlset>`;
 
     return new Response(xml, {
         headers: {
-            'Content-Type': 'application/xml',
-            'Cache-Control': 'no-cache'
+            "Content-Type": "application/xml; charset=UTF-8",
+            "Cache-Control": "no-cache"
         }
-    })
+    });
 }
