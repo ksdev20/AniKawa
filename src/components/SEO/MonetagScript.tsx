30a9f9e3a9dac3f4183fdd0cf2db4da2841cafd1
ksdev20 <batbale01@gmail.com>
2026-06-25 07:37:09 +0530

    Added Monetag Script and the multitag file

---
+++ src/components/SEO/MonetagScript.tsx
@@ -0,0 +1,12 @@
+export default function MonetagScript() {
+  return (
+    <div>
+      <script
+        src="https://quge5.com/88/tag.min.js"
+        data-zone="253278"
+        async
+        data-cfasync="false"
+      ></script>
+    </div>
+  );
+}
